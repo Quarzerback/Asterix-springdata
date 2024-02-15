97fa7d95b80b92465b8b55adfd4dd77b76291d88
Quarzerback <157362104+Quarzerback@users.noreply.github.com>
2024-02-15 10:54:34 +0100

    Initial commit

---
+++ src/main/java/de/quarzerback/asterixspringdata/AsterixSpringdataApplication.java
@@ -0,0 +1,13 @@
+package de.quarzerback.asterixspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsterixSpringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsterixSpringdataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/quarzerback/asterixspringdata/AsterixSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package de.quarzerback.asterixspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsterixSpringdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
